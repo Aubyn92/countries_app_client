768501e689a53d627667d14b281598f373fa7412
Aubyn92 <livstaubyn92@gmail.com>
2020-07-01 14:58:43 +1000

    Created two new components for 'protected route' and 'secrets

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,8 @@ import NoMatch from "./NoMatch";
 import Navbar from "../shared/Navbar";
 import AddCountry from "./AddCountry";
 import EditCountry from "./EditCountry";
+import ProtectedRoute from './ProtectedRoute'
+import Secrets from './Secrets'
 
 class App extends React.Component {
   render() {
@@ -13,6 +15,7 @@ class App extends React.Component {
       <>
         <Navbar />
         <Switch>
+          <ProtectedRoute exact path="/secrets" component={Secrets} />
           <Route exact path="/countries/:id/edit" component={EditCountry} />
           <Route exact path="/countries/create" component={AddCountry} />
           <Route exact path="/countries" component={Countries} />
